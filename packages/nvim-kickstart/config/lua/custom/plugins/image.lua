86f8e00a614b32f99e5c38d3e95a71ef2da2d50b
Daniel Grundler <danielgrundler@gmail.com>
2026-04-16 15:03:38 +0200

    Update treesitter

--- packages/nvim-kickstart/config/lua/custom/plugins/image.lua
+++ packages/nvim-kickstart/config/lua/custom/plugins/image.lua
@@ -2,9 +2,10 @@ return {
   {
     '3rd/image.nvim',
     opts = {
-      markdown = {
-        enabled = false,  -- This should fix the error
-        render_settings = {},
+      integrations = {
+        markdown = {
+          enabled = false,  -- This should fix the error
+        },
       },
     },
   },
